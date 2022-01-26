559b13a069663e65d1eb8f73cd84e7e786ab699f
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2022-01-26 21:32:43 +0530

    App

--- src/App.js
+++ src/App.js
@@ -4,7 +4,40 @@ const App = () => {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
-  return <div>Hello</div>;
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch("https://restcountries.com/v2/all")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {items.map((item) => (
+          <li key={Math.random()}>{item.name}</li>
+        ))}
+      </ul>
+    );
+  }
 };
 
 export default App;
