f30c4bccf4dd3a0e07e440b4b6e5de847a0b3346
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2022-01-26 21:31:41 +0530

    App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
   return <div>Hello</div>;
 };
 
